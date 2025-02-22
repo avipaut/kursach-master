a6e9900443fe76fd925e2ca4258ca32cd2a5d6de
medeer <telmanov.meder19@gmail.com>
2025-02-23 00:22:52 +0600

    Удаление ZOOM ключей

---
+++ routes/config_zoom.py
@@ -0,0 +1,8 @@
+# ZOOM_ACCOUNT_ID = "TVtxQB9USyuVsGdVCuV3_g"
+# ZOOM_CLIENT_ID = "xKEwCBkmT_6RSopEXBsgVQ"
+# ZOOM_CLIENT_SECRET = "JQxDCNRnnhA8vDc5OoBLwcxiku7pg42E"
+
+class Config:
+    ZOOM_CLIENT_ID = "xKEwCBkmT_6RSopEXBsgVQ"  # Получите в Zoom Marketplace
+    ZOOM_CLIENT_SECRET = "JQxDCNRnnhA8vDc5OoBLwcxiku7pg42E" # Получите в Zoom Marketplace
+    ZOOM_ACCOUNT_ID = "TVtxQB9USyuVsGdVCuV3_g"  # Получите в настройках аккаунта Zoom
