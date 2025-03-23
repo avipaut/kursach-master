86cfd846b8721a5e1cb4e32a27f1d8b5dc1d0bc0
avipaut <kiril.com1310@gmail.com>
2025-03-24 01:26:13 +0300

    update

--- app.py
+++ app.py
@@ -194,4 +194,4 @@ with app.app_context():
         print("Пользователь admin не найден")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
